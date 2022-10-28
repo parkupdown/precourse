7ece5eb74a4d3fb7e3220d335c412d3cc5aa2c0d
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-10-28 20:26:04 +0900

    4번 모듈화성공

--- afterStart/1주차/4번모듈화.js
+++ afterStart/1주차/4번모듈화.js
@@ -41,63 +41,6 @@ function arrayToString(wordArrays) {
 }
 
 function problem4(word) {
-  const upperWord = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  const lowerWord = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
   const reverseWordArray = reverseWord(upperWord, lowerWord);
   let wordArrays = wordToArray(word);
   checkLowerCase(wordArrays, reverseWordArray[0], lowerWord);
---
+++ afterStart/1주차/4번모듈화수정.js
@@ -0,0 +1,123 @@
+const word = "I Love You";
+
+function alphabetGather() {
+  const upperWord = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const lowerWord = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return [lowerWord, upperWord];
+}
+
+function reverseWord() {
+  const [lowerWord, upperWord] = alphabetGather();
+  const revLowerCase = [...lowerWord].reverse();
+  const revUpperCase = [...upperWord].reverse();
+  return [revLowerCase, revUpperCase];
+}
+
+function wordToArray(word) {
+  let wordArrays = word.split("");
+  return wordArrays;
+}
+
+function checkVaccumCase(word) {
+  let vaccumChecked = wordToArray(word);
+  vaccumChecked.forEach((item, index) => {
+    if (item === " ") {
+      vaccumChecked[index] = " ";
+    }
+  });
+  return vaccumChecked;
+}
+
+function checkLowerCase(word) {
+  let lowerchecked = checkVaccumCase(word);
+  const [revLowerCase, revUpperCase] = reverseWord();
+  const [lowerWord, upperWord] = alphabetGather();
+  lowerchecked.forEach((item, index) => {
+    if (item !== " " && item.toLowerCase() === item) {
+      let wordLowerIndex = lowerWord.indexOf(item);
+
+      lowerchecked[index] = revLowerCase[wordLowerIndex];
+    }
+  });
+  return lowerchecked;
+}
+
+function checkUpperCase(word) {
+  let upperchecked = checkLowerCase(word);
+  const [lowerWord, upperWord] = alphabetGather();
+  const [revLowerCase, revUpperCase] = reverseWord();
+  upperchecked.forEach((item, index) => {
+    if (item !== " " && item.toUpperCase() === item) {
+      let wordUpperIndex = upperWord.indexOf(item);
+      upperchecked[index] = revUpperCase[wordUpperIndex];
+      //대문자찾기
+    }
+  });
+  return upperchecked;
+}
+
+function arrayToString(word) {
+  let wordJoined = checkUpperCase(word);
+  wordJoined = wordJoined.join("");
+  return wordJoined;
+}
+
+function problem4(word) {
+  const answerWord = arrayToString(word);
+  return answerWord;
+}
+problem4(word);
